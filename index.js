19304f0b302a735810a71cab3329805127e60911
Cássio Zen <cassiozen@gmail.com>
2017-12-29 14:31:05 -0500

    Starting point

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
-const {promisify} = require('util');
 const fs = require('fs');
-const readFileAsync = promisify(fs.readFile);
 
-const mostFrequentWord = (text) => {
-  const words = text.toLowerCase().match(/[^_\W]+/g);
-  const tally = {};
-  let mostFrequentWord = null;
-  
-  words.forEach(word => {
-    tally[word] = (tally[word] || 0) + 1 ;
-    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
-      mostFrequentWord = word;
-  });
-  return mostFrequentWord;
-}
+const findPassword = () => {
+  // Your code here
 
-const findPassword = async () => {
-  const poem1 = await readFileAsync("poems/starting-poem.txt", "utf-8");
-  const poem2FileName = mostFrequentWord(poem1);
-  const poem2 = await readFileAsync(`poems/${poem2FileName}.txt`, "utf-8");
-  const poem3FileName = mostFrequentWord(poem2);
-  const poem3 = await readFileAsync(`poems/${poem3FileName}.txt`, "utf-8");
-  const password = mostFrequentWord(poem3);
-  console.log(`The password is: ${password}`);
 }
 
 findPassword();
