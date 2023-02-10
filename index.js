dc8754ec22c9a2124648b54ecb04d8a68f8102b5
Logan <logancable@ymail.com>
2023-02-10 10:57:57 -0800

    commit 2

--- index.js
+++ index.js
@@ -17,8 +17,11 @@ const findPassword = async () => {
   // Your code goes here
   const poem1 = await readFile("poems/starting-poem.txt", "utf-8")
   const poem2FileName = `poems/${mostFrequentWord(poem1)}.txt`
-  const poem3 = `poems/${mostFrequentWord(happy)}.txt`
-  console.log(poem3)
+  const poem2Read = await readFile("poems/happy.txt", "utf-8")
+  const poem3 = `poems/${mostFrequentWord(poem2Read)}.txt`
+  const poem3Read = await readFile("poems/you.txt", "utf-8")
+  const password = `poems/${mostFrequentWord(poem3Read)}.txt`
+  console.log(password)
 }
 
 findPassword();
